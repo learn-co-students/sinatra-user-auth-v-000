64df0a20550dae92e8991f79f3a3b5c3907022e3
Shana L Moore <shana.lavina.moore@gmail.com>
2016-01-02 16:29:53 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,12 +22,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/sessions/login' do
-
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    
+    @user = User.find_by(email: params[:email], password: params[:password])
+    session[:id] = @user.id    
     redirect '/users/home'
   end
 
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-   
+    @user = User.find(session[:id])   
     erb :'/users/home'
   end
 
