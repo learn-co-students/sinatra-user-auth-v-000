5f04dc4fa6599846be076209873b9a6ed091792e
glee38 <glee3388@gmail.com>
2016-04-01 21:35:10 -0400

    add notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,9 @@ class ApplicationController < Sinatra::Base
     erb :'/registrations/signup'
   end
 
-  post '/registrations' do    
+  post '/registrations' do   
+    # use the data in params to create a new user and log them in by
+    # setting the session[:id] equal to the user's id here 
     @user = User.create(name: params[:name], email: params[:email], password: params[:password])
     session[:id] = @user.id
     redirect '/users/home'
@@ -28,17 +30,28 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+    # find the user who submitted the log in forms by looking in your database 
+    #   for the user with the email and password from the params
+    # sign them in by setting the session[:id] equal to the user's id
+
+    # redirect the user to this route: get '/users/home' 
+    #  that route is in the Users Controller. Go check out the code there. 
     @user = User.find_by(email: params[:email], password: params[:password])
     session[:id] = @user.id
     redirect '/users/home'
   end
 
   get '/sessions/logout' do 
+    # log out the user by clearing the session hash here
     session.clear
     redirect '/'
   end
 
   get '/users/home' do
+    # find the current user by finding the user with the id that is stored 
+    #   in session[:id]
+    # set that user equal to a variable, @user, so that the view found in 
+    #   app/views/users/home.erb can render that user
     @user = User.find(session[:id])
     erb :'/users/home'
   end
