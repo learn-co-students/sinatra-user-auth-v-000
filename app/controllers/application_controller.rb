cbbbcfb8b7eddfd7b5a7a55b63dd64537edd6cad
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-04-14 19:38:57 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,33 +12,37 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
-  post '/registrations' do 
+  post '/registrations' do
+    #binding.pry 
+    @user = User.find_or_create_by(:name => params[:name], :email => params[:email], :password => params[:password])
+    @user.save
+
+    session[:id] = @user.id
+
     redirect '/users/home'
   end
 
   get '/sessions/login' do
-
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    
+    @user = User.find_or_create_by(email: params[:email], password: params[:password])
+    @user.id = session[:id]
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
-   
+    @user = User.find_by(session[:id])
     erb :'/users/home'
   end
-
 
 end
