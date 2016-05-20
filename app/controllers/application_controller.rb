ae0b2d041a4755bf639ddfdfd77688cc4561c12e
jbok4 <jbok4@aol.com>
2016-05-20 13:33:19 -0400

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do 
-    erb :home
+    erb :'/home'
   end
 
   get '/registrations/signup' do
@@ -28,17 +28,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
   get '/sessions/logout' do 
-
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
-   
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
