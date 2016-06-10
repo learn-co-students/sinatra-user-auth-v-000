0fa1c83491e17b8f3f9a49fa0e768e1ebbacd6e0
kade042 <dereje88@gmail.com>
2016-06-10 04:15:39 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,8 @@ ENV['SINATRA_ENV'] ||= "development"
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
+  #database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
+  :database => "db/user_authdevelopment.sqlite"
 )
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
