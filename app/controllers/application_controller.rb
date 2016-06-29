b2dfa4a2e3f955abb081c54a5642e04892665c3e
wook kim <wooke2e@gmail.com>
2016-06-29 18:37:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,12 +30,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
 
     redirect '/users/home'
   end
 
   get '/sessions/logout' do
-
+    session.clear
     redirect '/'
   end
 
