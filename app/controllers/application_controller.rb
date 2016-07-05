45beab2d1787bc42984507254dc7c869b599203b
jwolfe890 <jwolfe890@gmail.com>
2016-07-04 21:47:38 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do 
+    session.clear
     erb :home
   end
 
@@ -19,7 +20,7 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params[:name], email: params[:email], password: params[:password])
         # binding.pry 
     @user.save
-        session[:id] = @user.id
+    session[:id] = @user.id
 
     redirect '/users/home'
   end
@@ -29,7 +30,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
@@ -39,7 +41,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-   
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
