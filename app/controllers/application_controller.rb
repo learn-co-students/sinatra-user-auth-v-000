da10d2f4258a0f36ee2e7e320b7cffaa53d11dec
johnsontaiwo <josseytee@gmail.com>
2016-07-25 22:01:10 -0400

    modified files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do 
-    puts params
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20160726012007) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
+
 end
