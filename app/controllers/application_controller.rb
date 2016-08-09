ccb7916e6f89cac17e05053aed8fb81e4df3299a
Jesse Novotny <jessenovotny@gmail.com>
2016-08-09 12:58:44 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,35 +8,36 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do 
+    @session = session
     erb :home
   end
 
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
   post '/registrations' do 
+    @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
   get '/sessions/login' do
-
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    
+    #add user.id to @session[:id]
     redirect '/users/home'
   end
 
   get '/sessions/logout' do 
-
+    #clears sessions hash
     redirect '/'
   end
 
   get '/users/home' do
-   
+    # find user by id and store in @user 
     erb :'/users/home'
   end
 
