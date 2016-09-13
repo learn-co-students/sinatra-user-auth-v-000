66a6a1adae588ae6dd4c83f6bf67b4a26820e596
nicolefederici <nicolefederici@yahoo.com>
2016-09-13 13:50:34 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,9 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
 )
+# configure :development do
+#   set :database, 'sqlite3:db/database.db'
+# end
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
