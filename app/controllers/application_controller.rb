0ecc11be12a15073eb80dd5a57a064344e9ba56a
bryanhou1 <Hou@Bryans-MacBook-Pro.local>
2017-04-24 19:25:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,3 +43,5 @@ class ApplicationController < Sinatra::Base
   end
 
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424050933) do
+ActiveRecord::Schema.define(version: 20170424231807) do
+
+  create_table "user", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
