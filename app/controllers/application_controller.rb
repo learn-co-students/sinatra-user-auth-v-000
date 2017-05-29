3f040fadf423608506b74a9697be6f0e8b4449b0
WilliamBarela <william.barela.dev@gmail.com>
2017-05-29 14:57:42 +0900

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
