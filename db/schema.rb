9586b5c99a1fc532461c1e9e49ffa18c2d1dc163
Levio Marotti III <lm3@protonmail.com>
2017-06-06 20:07:28 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607004859) do
+ActiveRecord::Schema.define(version: 20170607010553) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
