cf333e7a6a343eabc393413dc102914f84002041
Joseph Hyatt <jhyatt1017@gmail.com>
2017-06-11 23:30:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,35 +7,44 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
+#render the sign-up form view.
   get '/registrations/signup' do
     erb :'/registrations/signup'
   end
 
+#handling the POST request that is sent when a user hits 'submit' on the sign-up form.
   post '/registrations' do
-    
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
+#rendering the login form.
   get '/sessions/login' do
     erb :'sessions/login'
   end
 
+#receiving the POST request that gets sent when a user hits 'submit' on the login form.
   post '/sessions' do
-    
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
-
+#logging the user out by clearing the session hash.
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
+#rendering the user's homepage view.
   get '/users/home' do
-   
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+#validate some of the attributes of our user by writing code that makes sure no one can sign up without inputting their name, email, and password. More on this later.
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
 end
