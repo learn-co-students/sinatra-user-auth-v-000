94f8d55c25bcf94e1e51093ed30b475179d7dff4
Sohee Cho <soheecho94@gmail.com>
2017-08-09 11:37:05 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  #validate some of the attributes of our user by writing code that makes sure
+  #no one can sign up without inputting their name, email, and password.
   validates_presence_of :name, :email, :password
 end
