6ff87de45add407b12a3dd8d6a09aec14167c06b
Annette Maza <nettymaza@gmail.com>
2017-10-10 16:58:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,35 +7,44 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
+  #Renders sign-up form view, found in signup.erb
   get '/registrations/signup' do
     erb :'/registrations/signup'
   end
 
+  #Handles post request, when user hits 'submit' on signup form.
   post '/registrations' do
-    
+    @user = User.new(name: params['name'], email: params['email'], password: params['password'])
+    @user.save
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
+  #Renders the login form
   get '/sessions/login' do
     erb :'sessions/login'
   end
 
+  #Handles post request that gets sent when user hits 'submit' on login form.
   post '/sessions' do
-    
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
-
+  #Logs user out by clearing the session hash.
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
+  #Renders user's home page.
   get '/users/home' do
-   
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
