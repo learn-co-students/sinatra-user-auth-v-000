e5785f27e26a589f855a442787a58f1a93e2285f
aisayo <aisayo@icloud.com>
2017-10-11 19:42:05 +0000

    Done.

---
+++ db/migrate/20171011190250_create_user.rb
@@ -0,0 +1,12 @@
+class CreateUser < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
+  end
+
+  def down
+  end
+end
