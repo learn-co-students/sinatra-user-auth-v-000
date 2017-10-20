35858d86f8964eb3c11b90a86d4e389495d85fdb
Elvin <Dimix181@Gmail.com>
2017-10-20 15:33:00 -0400

    all test are passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
-    erb :home
+  get '/' do
+    erb :'/home'
   end
 
   get '/registrations/signup' do
@@ -16,26 +16,31 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user.save
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
   get '/sessions/login' do
+
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    
+    @user = User.find_by(email: params[:email], password: params[:password])
+    session[:id] = @user.id
+
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
-   
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
