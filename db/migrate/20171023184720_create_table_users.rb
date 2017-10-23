1d0e6012c3b107a79791fe4859b0a410a118e7b9
Justin Daniel <justinddaniel@gmail.com>
2017-10-23 20:53:13 +0000

    Done.

---
+++ db/migrate/20171023184720_create_table_users.rb
@@ -0,0 +1,9 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users do |u|
+      u.string :name
+      u.string :email
+      u.string :password
+    end
+  end
+end
