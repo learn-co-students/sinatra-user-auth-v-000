07ad6133670af6a70ef0a6a0b730583e5e96f599
Jessie Huff <huffjd1@gmail.com>
2017-11-02 19:03:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,19 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
-  get '/registrations/signup' do
+  get '/registrations/signup' do #responsible for rendering the sign-up template
     erb :'/registrations/signup'
   end
 
   post '/registrations' do
-    
+    puts params
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
@@ -25,17 +28,19 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    
+    puts params
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
-   
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
