481b05c3e81a975003cca3e75a4333309ae8601c
github username <github email address>
2017-11-04 18:11:12 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,8 @@ class ApplicationController < Sinatra::Base
     #should get the new user's info from params hash
     #create a new user
     #sign the user in
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
+    @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+    # @user.save
     session[:id] = @user.id
     redirect '/users/home' #redirect somewhere else, e.g. users home page
   end
