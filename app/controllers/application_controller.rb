69803b50444cd53223f29a386decc42dd20e0f91
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-24 03:51:30 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
@@ -16,26 +16,28 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    
+    #user info into params hash, create new user, sign them in, redicret
     redirect '/users/home'
   end
 
+#render login form
   get '/sessions/login' do
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    
+    #receive Post request, code to grab users info from params hash, match that info against the etnries in db, and if ===, sign in user
+
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    #clear the session hash
     redirect '/'
   end
 
   get '/users/home' do
-   
+   #render user's homepage view 
     erb :'/users/home'
   end
 
