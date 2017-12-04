82ae165377e2346764b71b621398de457f770682
Naomi Dennis <njdennis94@gmail.com>
2017-12-04 17:29:09 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171204170700) do
+
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.text "email"
+    t.text "password"
+  end
 
 end
