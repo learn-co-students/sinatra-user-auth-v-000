e7b3e9fc3062e74b2a8c4857d7cb42e100cf13b6
Richard Chang <rchang9999@yahoo.com>
2018-01-18 22:43:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
@@ -16,8 +16,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:id] = @user.id
     redirect '/users/home'
+  end
+
+
+  get '/users/home' do
+    @user = User.find(session[:id])
+    erb :'/users/home'
   end
 
   get '/sessions/login' do
@@ -25,18 +33,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    
-    redirect '/users/home'
+    @user = User.find_by(email: params["email"], password: params["password"])
+    session[:id] = @user.id
+     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
-  get '/users/home' do
-   
-    erb :'/users/home'
-  end
+
 
 end
