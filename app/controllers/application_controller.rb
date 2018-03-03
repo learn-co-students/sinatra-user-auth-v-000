4440f2b8ffb1e1606ac6c941ee183df82e29d57f
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-03-03 15:33:16 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
@@ -16,7 +16,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    
+    @user = User.create(params)
+    session[:id] = @user.id
+
     redirect '/users/home'
   end
 
@@ -25,17 +27,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    
+    @user = User.find_by(params)
+    session[:id] = @user.id
+
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
+    session.clear
 
     redirect '/'
   end
 
   get '/users/home' do
-   
+    @user = User.find(session[:id])
+
     erb :'/users/home'
   end
 
