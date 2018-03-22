ba6779ec8af70abd8950997b45bb71df45e48548
ElleeB <laura.trager@gmail.com>
2018-03-22 10:31:34 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,29 +12,41 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
+
     erb :'/registrations/signup'
   end
 
   post '/registrations' do
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+
+    session[:id] = @user.id
 
     redirect '/users/home'
   end
 
   get '/sessions/login' do
+
     erb :'sessions/login'
   end
 
   post '/sessions' do
 
-    redirect '/users/home'
+    if @user = User.find_by(email: params["email"], password: params["password"])
+      session[:id] = @user.id
+      redirect '/users/home'
+    else
+      redirect '/sessions/login'
+    end
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
+    @user = User.find(session[:id])
 
     erb :'/users/home'
   end
