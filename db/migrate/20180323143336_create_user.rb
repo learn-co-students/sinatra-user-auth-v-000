aa1ab67dd7f7f961aa196cb0db715d7fa96c0b6e
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-23 14:58:31 +0000

    Done.

---
+++ db/migrate/20180323143336_create_user.rb
@@ -0,0 +1,10 @@
+
+class CreateUser < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
+  end
+end
