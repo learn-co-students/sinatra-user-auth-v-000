757d64c4ca89da3496650eea7d7a593e6595a463
Araba Taylor <araba.taylor20@gmail.com>
2018-04-04 23:36:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,21 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    #render the home page view
     erb :home
   end
 
   get '/registrations/signup' do
+    #renders sign-up view
     erb :'/registrations/signup'
   end
 
   post '/registrations' do
+    #should handle 'POST' request from sign-up form
+    #and contain code that takes info from params
+    #creates a new user
+    #signs in the new user
+    #then redirects them to a new location
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:id] = @user.id
@@ -23,19 +30,32 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/sessions/login' do
+    #renders login form
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    redirect '/users/home'
+    #receives post request from log-in form
+    #grabs user's info from params
+    #matchs info against exsiting entries in user database
+    #if matching entry is found, user is signed in
+    @user = User.find_by(email: params["email"], password: params["password"])
+    if @user
+      session[:id] = @user.id
+      redirect '/users/home'
+    else
+      redirect '/sessions/login'
+    end
   end
 
   get '/sessions/logout' do
+    #should log the user out by clearing the 'session' hash
     session.clear
     redirect '/'
   end
 
   get '/users/home' do
+    #render homepage view
     @user = User.find(session[:id])
     erb :'/users/home'
   end
