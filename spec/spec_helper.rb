e987ed2a4501a08a6375b8bb44466f3823b4b5c6
Chaim Reisman <crreisman@gmail.com>
2018-04-10 16:14:52 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'capybara/rspec'
 require 'capybara/dsl'
 
 #if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
-  #raise 'Migrations are pending run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
+#  raise 'Migrations are pending run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
 #end
 
 RSpec.configure do |config|
