84e327cad31136c543161766e38716bed54efa52
Aspen James <aspenjames@tqca.org>
2018-05-03 21:31:51 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:id] = @user.id
 
     redirect '/users/home'
   end
@@ -25,17 +28,24 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+    @user = User.find_by(email: params["email"], password: params["password"])
 
-    redirect '/users/home'
+    if @user.nil?
+      redirect '/sessions/login'
+    else
+      session[:id] = @user.id
+      redirect '/users/home'
+    end
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
+    session.clear
 
     redirect '/'
   end
 
   get '/users/home' do
-
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
