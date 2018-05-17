e3122edf6e2a9b4ca543ca4ba327a1d48b9eb31e
Raiet Intraub <raietsaeed@gmail.com>
2018-05-17 01:12:30 +0000

    Done.

---
+++ db/migrate/20180517003406_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |u|
+      u.string :name
+      u.string :email
+      u.string :password
+    end
+  end
+end
