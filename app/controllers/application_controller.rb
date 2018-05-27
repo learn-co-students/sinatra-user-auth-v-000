3126bb94a10e0aa9deee216ea5d6c20bc6c30151
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-27 23:25:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-
+    @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+    session[:user_id] = @user.id
     redirect '/users/home'
   end
 
@@ -29,7 +30,7 @@ class ApplicationController < Sinatra::Base
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
 
     redirect '/'
   end
