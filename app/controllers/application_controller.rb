570f5ab26eabada551f91e5bed8267cbd172898f
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-28 00:36:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,25 +17,34 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+    @user.save
     session[:user_id] = @user.id
     redirect '/users/home'
   end
 
   get '/sessions/login' do
+
     erb :'sessions/login'
   end
 
   post '/sessions' do
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    else
+      redirect '/sessions/login'
+    end
 
-    redirect '/users/home'
   end
 
   get '/sessions/logout' do
-
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
+    @user = User.find(session[:user_id])
 
     erb :'/users/home'
   end
