0f017883e5f16cdce517837839ef301dfbb69e6e
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-16 21:23:08 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,5 @@ ActiveRecord::Base.establish_connection(
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
+
+require_all 'app'
