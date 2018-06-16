ba6c9e352ebb2759d203f0a7bc86670e39d786b9
thesilentc <thesilentc@me.com>
2018-06-16 16:26:14 -0400

    nothing a 'rake db:migrate SINATRA_ENV=test' couldn't fix lol

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 source "https://rubygems.org"
 
 gem 'sinatra'
-gem 'activerecord', '4.2', :require => 'active_record'
+gem 'activerecord', '4.2'
 gem 'sinatra-activerecord'
 gem 'rake'
 gem 'require_all'
@@ -10,11 +10,10 @@ gem 'sqlite3'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
-gem 'tux'
-gem 'rspec'
 
 group :test do
   gem 'launchy'
+  gem 'rspec'
   gem 'capybara'
   gem 'rack-test'
   gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,37 +16,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
-
-    session[:id] = @user.id
+    @user = User.find_by(email: params[:email])
+    session[:user_id] = @user.id
     redirect '/users/home'
   end
 
-  post '/sessions' do
-    if @user = User.find_by(email: params["email"], password: params["password"])
-      session[:id] = @user.id
-
-      redirect '/users/home'
-    else
-      redirect '/sessions/login'
-    end
-  end
-
   get '/sessions/login' do
-
     erb :'sessions/login'
   end
 
-
+  post '/sessions' do
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
+  end
 
   get '/sessions/logout' do
-
     redirect '/'
   end
 
   get '/users/home' do
-    @user = User.find(session[:id])
+    @user = User.find(session[:user_id])
     erb :'/users/home'
   end
 
