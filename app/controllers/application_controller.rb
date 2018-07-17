022b62c9d7fc2ccb231ee56fea0288a7583c436a
Elaine Anderson <eparie@bu.edu>
2018-07-17 20:01:31 +0000

    checks password validity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,11 +28,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(email: params["email"], password: params["password"])
+  if @user = User.find_by(email: params["email"], password: params["password"])
 session[:id] = @user.id
 
     redirect '/users/home'
+  else
+    redirect "/sessions/login"
   end
+end
 
   get '/sessions/logout' do
 session.clear
