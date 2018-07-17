1a7d049c69269c2d0dd11e22d45d87ca22e17c66
Judy McGoldrick <jjeanius@verizon.net>
2018-07-17 04:41:57 +0000

    Done.

--- db/migrate/20180716043001_users.rb
+++ db/migrate/20180716043001_users.rb
@@ -1,6 +1,10 @@
 class Users < ActiveRecord::Migration[5.2]
+<<<<<<< HEAD
   def change
     create_table :users do|t|
+=======
+  def create_table user do |t|
+>>>>>>> a70b9b505f25fa36f3c4573a64c24b0cefd3ef15
       t.string :name
       t.string :email
       t.string :password
