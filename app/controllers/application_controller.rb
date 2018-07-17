d59d881830646691d4665502daafba4d4c39b4a3
Judy McGoldrick <jjeanius@verizon.net>
2018-07-17 17:45:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,35 +18,39 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
     @user.save
     session[:id] = @user.id
 
-   redirect '/users/home'
+    redirect '/users/home'
+end
+
+   get '/sessions/login' do
+
+     erb :'/sessions/login'
    end
 
-  get '/sessions/login' do
-
-    erb :'sessions/login'
-  end
-
   post '/sessions' do
-    @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
+     @user = User.find_by(email: params["email"], password: params["password"])
+     if @user
+       session[:id] = @user.id
 
-    redirect '/users/home'
-  end
+       redirect '/users/home'
+     end
 
-  get '/sessions/logout' do
-      session.clear
+       redirect '/sessions/login'
+   end
 
-      redirect '/'
-  end
+   get '/sessions/logout' do
+     session.clear
 
-  get '/users/home' do
-    @user = User.find(session[:id])
+     redirect '/'
+   end
 
-    erb :'/users/home'
-  end
+    get '/users/home' do
+      @user = User.find(session[:id])
+
+      erb :'/users/home'
+    end
 
 end
