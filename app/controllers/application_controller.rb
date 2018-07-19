d6cb4af2a656a644848b007eaa8bb06c4cb8565c
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-19 04:56:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-
+    puts params
     redirect '/users/home'
   end
 
@@ -25,11 +25,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-
+    puts params
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
 
     redirect '/'
   end
