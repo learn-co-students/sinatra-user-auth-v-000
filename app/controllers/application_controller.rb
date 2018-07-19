584780d620af89de5f5901381584be08995d4212
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-19 18:18:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    puts params
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
@@ -25,6 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+    binding.pry
     puts params
     redirect '/users/home'
   end
@@ -35,7 +38,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
+    @user =
     erb :'/users/home'
   end
 
