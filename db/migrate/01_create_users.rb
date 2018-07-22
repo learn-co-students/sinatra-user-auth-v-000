63477f415e8e7701a28910e22d9c4fc18e916be3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-22 14:28:58 +0000

    Done.

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,9 +1,10 @@
-class CreateUser < ActiveRecord::Migration[5.1]
+class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
       t.string :password
       t.string :email
+
     end
   end
 end
