9d7c2c67617d7a2f0ea3d337fe266747c5a3a95a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-22 15:00:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,40 +3,44 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
-    enable :sessions
-    set :session_secret, "secret"
+      enable :sessions
+      set :session_secret, "secret"
+    end
+
+    get '/' do
+      erb :home
+    end
+
+    get '/registrations/signup' do
+      erb :'/registrations/signup'
+    end
+
+    post '/registrations' do
+      @user = User.find_by(email: params[:email])
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+
+    get '/sessions/login' do
+      erb :'sessions/login'
+    end
+
+    post '/sessions' do
+      @user = User.find_by(email: params[:email], password: params[:password])
+      if @user
+        session[:user_id] = @user.id
+        redirect '/users/home'
+      end
+      redirect '/sessions/login'
+    end
+
+    get '/sessions/logout' do
+      redirect '/'
+    end
+
+    get '/users/home' do
+      @user = User.find(session[:user_id])
+      erb :'/users/home'
+    end
+
   end
-
-  get '/' do
-    erb :home
-  end
-
-  get '/registrations/signup' do
-    erb :'/registrations/signup'
-  end
-
-  post '/registrations' do
-
-    redirect '/users/home'
-  end
-
-  get '/sessions/login' do
-    erb :'sessions/login'
-  end
-
-  post '/sessions' do
-
-    redirect '/users/home'
-  end
-
-  get '/sessions/logout' do 
-
-    redirect '/'
-  end
-
-  get '/users/home' do
-
-    erb :'/users/home'
-  end
-
-end
