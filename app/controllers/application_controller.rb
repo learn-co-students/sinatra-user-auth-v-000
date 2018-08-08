2b45cb50f9fd37f29478a9e14afeac98995d3cbd
maxwellbenton <maxwellbenton@gmail.com>
2018-08-08 16:46:51 -0400

    updated solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,19 +17,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-<<<<<<< HEAD
-    # use the data in params to create a new user and log them in by
-    # setting the session[:id] equal to the user's id here
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
-    session[:id] = @user.id
-
-    # this redirect takes us to the route: get '/users/home' that is in the Users Controller
-    #   go and look at that route in the Users Controller.
-=======
     @user = User.find_by(email: params[:email])
     session[:user_id] = @user.id
->>>>>>> solution updated and working
+
     redirect '/users/home'
   end
 
