09a10873d3fe48a3e8d04585770f4598f5b9bd2d
geoffrey woodcock <theatomproject@gmail.com>
2018-08-09 18:34:40 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
+  
+    def change
+    create_table :user do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
+  end
 end
