b86b4a100d7cedb0625dac710b96cefdca0a3a12
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-18 20:59:47 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20180818205805) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
+
 end
