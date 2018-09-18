b5c7bafbf963655ec50544c174e0c98af6a76eea
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-18 03:10:46 +0000

    Done.

--- Rakefile
+++ Rakefile
@@ -1,4 +1,4 @@
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
-
-# Type `rake -T` on your command line to see the available rake tasks.
+require 'sinatra/activerecord'
+require './app'
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180918024128) do
 
 end
