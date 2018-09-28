bdc2ec8a60a92379b2b8448f48e600a3fd7e2d40
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-09-28 03:29:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-
+    @user = User.create(name: params["name"], email: params["email"], password: params["password"])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
@@ -25,18 +26,23 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-
-    redirect '/users/home'
+    @user = User.find_by(email: params["email"], password: params["password"])
+    if @user
+      session[:id] = @user.id
+      redirect '/users/home'
+    else
+      redirect '/sessions/login'
+    end
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
-
-    erb :'/users/home'
+    @user = User.find(session[:id])
+      erb :'/users/home'
   end
 
 end
