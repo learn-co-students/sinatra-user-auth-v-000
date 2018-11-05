7fe24d09021742ee6187395461bb67b45c565e62
Sherry Martin <sm674@cornell.edu>
2018-11-05 15:56:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,13 +23,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/sessions/login' do
-    erb 'sessions/login'
+    erb :'sessions/login'
   end
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
-    redirect '/users/home'
+    if @user
+      session[:id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
   end
 
   get '/sessions/logout' do
