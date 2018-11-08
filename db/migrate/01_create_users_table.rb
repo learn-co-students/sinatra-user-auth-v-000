52c4b03b61e21d382131414f76d3ef024e1db821
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-11-08 16:00:16 +0000

    Done.

---
+++ db/migrate/01_create_users_table.rb
@@ -0,0 +1,11 @@
+class CreateUsersTable < ActiveRecord::Migration
+  
+  def change
+    create_table :users do |u|
+      u.string :name
+      u.string :email
+      u.string :password
+    end
+  end
+  
+end
