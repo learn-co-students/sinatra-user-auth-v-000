96dfa6d761004480ea7821e4e565784c97985390
Romeo Gono <romeogono1@aol.com>
2018-12-16 00:13:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
-    session[:id] = @user.id
+    session[:user_id] = @user.id
     redirect '/users/home'
   end
 
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-   session[:id] = @user.id
+   session[:user_id] = @user.id
     redirect '/users/home'
   end
 
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-    @user = User.find(session[:id])
+    @user = User.find(session[:user_id])
     erb :'/users/home'
   end
 
--- db/migrate/20181215111957_users.rb
+++
@@ -1,9 +0,0 @@
-class Users < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-    end
-  end
-end
