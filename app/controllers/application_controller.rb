ce887d2114ecd854ea347a2087c1d96c83c426f4
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-15 17:31:39 -0500

    Add notes for further feature additions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    # binding.pry
+    
     if @user == nil
       redirect '/sessions/login'
     else
