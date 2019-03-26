f9529565645e329297e8aefc78c03a70bfcd875c
Cheryl K <cherylkelson@icloud.com>
2019-03-26 16:39:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,52 +2,54 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, "../views/") }
 
+
   configure do
     enable :sessions
     set :session_secret, "secret"
   end
 
+
   get '/' do
     erb :home
   end
 
-  get '/registrations/signup' do
 
+  get '/registrations/signup' do
     erb :'/registrations/signup'
   end
 
-  # def params
-  # @params
-  # end
-
 
   post '/registrations' do
-    @params = params
     puts params
-    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
     redirect '/users/home'
   end
 
+
   get '/sessions/login' do
     erb :'sessions/login'
   end
 
+
   post '/sessions' do
-    @user = User.find_by(name: params[:name], email: params[:email], password: params[:password])
-    if @user
+    puts params
+    @user = User.find_by(email: params["email"], password: params["password"])
+        if @user
       session[:user_id] = @user.id
       redirect '/users/home'
     end
     redirect '/sessions/login'
   end
 
+
   get '/sessions/logout' do
     session.clear
     redirect '/'
   end
-# binding.pry
+
+
   get '/users/home' do
     @user = User.find(session[:user_id])
     erb :'/users/home'
