97f39b8d5a55c7e265d1fee947f5fad5cd56e327
Diapause <diapause@protonmail.com>
2019-08-23 14:49:01 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
@@ -25,8 +24,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/sessions/login' do
-
-    # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
@@ -45,7 +42,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
     @user = User.find(session[:user_id])
     erb :'/users/home'
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  validates_presence_of :name, :email, :password
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
