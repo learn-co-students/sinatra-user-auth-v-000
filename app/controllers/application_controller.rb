1aa8bc28a637cacbc7b462fd3f2feef538a51a27
Tam Vu <vutam_a@hotmail.com>
2019-11-27 22:32:11 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+    puts params
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
