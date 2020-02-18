bbac05c5a3df86c7431d5b512f4ecf58eb14ba47
Jonathan Fernandez <jfern20@gmail.com>
2020-02-18 22:21:27 +0000

    Done.

--- db/migrate/create_users.rb
+++ db/migrate/create_users.rb
@@ -1,10 +1,9 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
-  
-  def change 
+class CreateUsers < ActiveRecord::Migration
+  def change
     create_table :users do |t|
-      t.string :name 
+      t.string :name
       t.string :email
-      t.string :password 
-   end 
-  end 
-end  
+      t.string :password
+    end
+  end
+end
