fc7776f281c7dc076bf5681ca04ad057218b2776
Eva Navarro <emanao@me.com>
2020-03-02 21:19:35 +0000

    Load first ApplicationController

--- config/environment.rb
+++ config/environment.rb
@@ -9,4 +9,4 @@ ActiveRecord::Base.establish_connection(
 )
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].sort.each {|f| require f}
