a864438f718499c49d9ecdef08855b7cfcf494c0
Daniel Suarez <suarez.daniel101@gmail.com>
2021-06-16 00:47:23 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :home
+
   end
 
   get '/registrations/signup' do
@@ -30,6 +31,8 @@ class ApplicationController < Sinatra::Base
     # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
+
+  
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
